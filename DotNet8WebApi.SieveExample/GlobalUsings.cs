a0f9c53e2d32191ab4420985f88fd2c21bec0c94
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-27 13:55:20 +0630

    Format Code

--- DotNet8WebApi.SieveExample/GlobalUsings.cs
+++ DotNet8WebApi.SieveExample/GlobalUsings.cs
@@ -1,6 +1,7 @@
 // Global using directives
 
 global using DotNet8WebApi.SieveExample.Db;
+global using DotNet8WebApi.SieveExample.Models;
 global using Microsoft.AspNetCore.Mvc;
 global using Sieve.Models;
 global using Sieve.Services;
