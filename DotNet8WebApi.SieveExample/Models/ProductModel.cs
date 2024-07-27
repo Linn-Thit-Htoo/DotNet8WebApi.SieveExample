c7f934ad0cddb144577e2f790cba575a75db3472
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-27 13:25:19 +0630

    Add project files.

---
+++ DotNet8WebApi.SieveExample/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using Sieve.Attributes;
+
+namespace DotNet8WebApi.SieveExample.Models
+{
+    public class ProductModel
+    {
+        [Sieve(CanSort = true)]
+        public int Id { get; set; }
+
+        [Sieve(CanFilter = true, CanSort = true)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Sieve(CanFilter = true, CanSort = true)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ DotNet8WebApi.SieveExample/Program.cs
@@ -0,0 +1,31 @@
+using DotNet8WebApi.SieveExample.Db;
+using Sieve.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<SieveProcessor>();
+builder.Services.AddSingleton<AppDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
